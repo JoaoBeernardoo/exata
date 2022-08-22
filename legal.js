737b7560bbc76b5912d780d55cc671b310a981d5
joao <joaobeernardoo@gmail.com>
2022-08-22 19:47:35 -0300

    completinho

---
+++ legal.js
@@ -0,0 +1,13 @@
+
+var hora = 20
+
+console.log(`agora sao exatamente ${hora}`)
+
+if(hora<12){
+    console.log('Bom dia!')
+}else if(hora < 17){
+    console.log('Boa tarde')
+
+}else if(hora <22){
+    console.log('Boa noite')
+}
